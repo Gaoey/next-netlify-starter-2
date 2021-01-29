3e2e642f4abad59d7ec9dee25ff8f0b014983a71
gaoey <gaoey@hotmail.com>
2021-01-29 17:27:50 +0700

    update nothing

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        
         <Link
           id="logo"
           href="https://cocky-curie-f05e0f.netlify.app/">
