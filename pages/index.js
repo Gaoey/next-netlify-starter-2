4ff92f988730504207dedcff846e223949a0f8ce
gaoey <gaoey@hotmail.com>
2021-01-29 17:30:07 +0700

    update Link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -15,7 +16,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-        
+
         <Link
           id="logo"
           href="https://cocky-curie-f05e0f.netlify.app/">
